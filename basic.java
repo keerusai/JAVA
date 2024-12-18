eff8e77eff14894ebd0c1e339523aee06260895a
keerusai <141726571+keerusai@users.noreply.github.com>
2024-12-18 19:54:42 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,7 @@
+public class basic {
+    //note:the class name will be save as the name file name
+    public static void main(String argsS[])
+    {
+        System.out.println("hello world!");
+    }
+}
