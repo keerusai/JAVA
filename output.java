67cb6cdad1506bf0f138ea539cc5dffac465227c
keerusai <141726571+keerusai@users.noreply.github.com>
2024-12-18 20:54:52 +0530

    Add files via upload

---
+++ output.java
@@ -0,0 +1,12 @@
+public class output {
+    public static void main(String args[])
+    {
+        /*  we will write the line of system.out.println("hello world");
+            * system=> actually the system is a final class which is from java standard library(i.e.from the java.lang package)which provides us the standard i/p,standard o/p and system resources
+            *out=> this is a common and regular system attribute from the system output stream. we can also say it is an example of PrintStream (Interface providing the capability for putting data into various output streams).
+            *println()=> whch prints the data into an output screen
+        */
+            System.out.println("hello");
+        
+    }
+}
